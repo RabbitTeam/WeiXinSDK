f4388fc6eab8445c0c49150ab1656ec9a9155502
majian159 <majian159@live.com>
2015-06-12 18:20:19 +0800

    优化。

--- Rabbit.WeiXin/Sample/Rabbit.WeiXin.Sample/Controllers/MutualController.cs
+++ Rabbit.WeiXin/Sample/Rabbit.WeiXin.Sample/Controllers/MutualController.cs
@@ -21,16 +21,16 @@ namespace Rabbit.WeiXin.Sample.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string signature, string timestamp, string nonce)
+        public ActionResult Index()
         {
             IHandlerBuilder builder = new HandlerBuilder();
 
             builder
                 .Use<SignatureCheckHandlerMiddleware>() //验证签名中间件。
-                .Use<CreateRequestMessageHandlerMiddleware>() //创建消息中间件。
+                .Use<CreateRequestMessageHandlerMiddleware>() //创建消息中间件（内置消息解密逻辑）。
                 .Use<IgnoreRepeatMessageHandlerMiddleware>() //忽略重复的消息中间件。
                 .Use<TestMessageHandlerMiddleware>() //测试消息处理中间件。
-                .Use<GenerateResponseXmlHandlerMiddleware>(); //生成相应XML处理中间件。
+                .Use<GenerateResponseXmlHandlerMiddleware>(); //生成相应XML处理中间件（内置消息加密逻辑）。
             //                            .Use<AgentHandlerMiddleware>(new AgentRequestModel(new Uri("http://localhost:22479/Mutual")));
 
             var context = new HandlerContext(Request);
