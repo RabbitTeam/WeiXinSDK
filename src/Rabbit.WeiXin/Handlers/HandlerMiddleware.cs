617c1eda59ba8022c8a92cf3b112d57a7330ee1f
zhaoxi <zhaoxiwork@126.com>
2017-12-22 17:21:56 +0800

    新增Middleware数据包

--- src/Rabbit.WeiXin/Handlers/HandlerMiddleware.cs
+++ src/Rabbit.WeiXin/Handlers/HandlerMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Rabbit.WeiXin.Handlers
 {
@@ -14,12 +15,33 @@ namespace Rabbit.WeiXin.Handlers
         protected HandlerMiddleware(HandlerMiddleware next)
         {
             Next = next;
+            MiddleBag = new Dictionary<string, object>();
+            if (next != null && MiddleBag.Count > 0)
+            {
+                foreach (var item in MiddleBag)
+                {
+                    if (next.MiddleBag.ContainsKey(item.Key))
+                    {
+                        next.MiddleBag[item.Key] = item.Value;
+                    }
+                    else
+                    {
+                        next.MiddleBag.Add(item.Key, item.Value);
+                    }
+                }
+
+            }
         }
 
         /// <summary>
         /// 下一个处理中间件。
         /// </summary>
         protected HandlerMiddleware Next { get; private set; }
+
+        /// <summary>
+        /// 中间件消息传递器
+        /// </summary>
+        protected Dictionary<string, object> MiddleBag { get; private set; } = new Dictionary<string, object>();
 
         /// <summary>
         /// 调用。
