7878427bfbffd570f57494f2dad2bc87fa5375f6
zhaoxi <zhaoxiwork@126.com>
2018-03-09 14:46:17 +0800

    新增序列化帮助类

---
+++ src/Rabbit.WeiXin/Utility/Extensions/SerializeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Rabbit.WeiXin.MP.Messages;
+using Rabbit.WeiXin.MP.Messages.Response;
+using Rabbit.WeiXin.MP.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rabbit.WeiXin.Utility.Extensions
+{
+    /// <summary>
+    /// 序列化对象
+    /// </summary>
+    public static class SerializeExtensions
+    {
+        /// <summary>
+        /// 序列化Response消息
+        /// </summary>
+        /// <param name="responseMessage">Response消息</param>
+        /// <returns></returns>
+        public static string Serialize(this IResponseMessage responseMessage)
+        {
+            var facotry = new ResponseMessageFactory(new MessageFormatterFactory());
+            return facotry.GetXmlByReponseMessage(responseMessage);
+        }
+    }
+}
