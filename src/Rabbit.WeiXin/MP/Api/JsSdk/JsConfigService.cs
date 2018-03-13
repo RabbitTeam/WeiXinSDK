05f1d8d86cfa6a68492bcaf4e623dc8225841385
zhaoxi <zhaoxiwork@126.com>
2018-03-13 11:39:54 +0800

    暂存

---
+++ src/Rabbit.WeiXin/MP/Api/JsSdk/JsConfigService.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using Rabbit.WeiXin.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rabbit.WeiXin.Utility.Extensions;
+
+namespace Rabbit.WeiXin.MP.Api.Js
+{
+
+    /// <summary>
+    /// Js配置生成生成服务
+    /// </summary>
+    public interface IJsConfigService
+    {
+        /// <summary>
+        /// 生成微信JsSDK配置信息
+        /// </summary>
+        /// <param name="currentUrl"></param>
+        /// <returns></returns>
+        JsSdkConfigModel Create(string currentUrl);
+    }
+
+
+    /// <summary>
+    /// Js配置生成生成服务
+    /// </summary>
+    public class JsConfigService : IJsConfigService
+    {
+        private readonly AccountModel _accountModel;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="accountModel"></param>
+        public JsConfigService(AccountModel accountModel)
+        {
+            _accountModel = accountModel;
+        }
+
+        public JsSdkConfigModel Create(string currentUrl)
+        {
+            JsSdkConfigModel result = new JsSdkConfigModel();
+            result.Timestamp = DateTimeHelper.GetTimeStampByTime(DateTime.Now).ToString();
+            result.AppId = _accountModel.AppId;
+            result.NonceStr = StringHelper.GetRandomString(32);
+             
+
+            return result;
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JsSdkConfigModel
+    {
+        /// <summary>
+        /// AppId
+        /// </summary>
+        [JsonProperty("appId")]
+        public string AppId { get; set; }
+
+        /// <summary>
+        /// 时间戳
+        /// </summary>
+        [JsonProperty("timestamp")]
+        public string Timestamp { get; set; }
+
+        /// <summary>
+        /// 随机字符串
+        /// </summary>
+        [JsonProperty("nonceStr")]
+        public string NonceStr { get; set; }
+
+        /// <summary>
+        /// Signature
+        /// </summary>
+        [JsonProperty("signature")]
+        public string Signature { get; set; }
+    }
+}
---
+++ src/Rabbit.WeiXin/Utility/StringHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rabbit.WeiXin.Utility
+{
+
+    /// <summary>
+    /// 字符串帮助类
+    /// </summary>
+    public class StringHelper
+    {
+        static Random random = new Random();
+        static string[] arr = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+
+        /// <summary>
+        /// 获得随机长度字符串
+        /// </summary>
+        /// <param name="length">字符串长度</param>
+        /// <returns></returns>
+        public static string GetRandomString(int length)
+        {
+            var result = new StringBuilder();
+            for (var i = 0; i < length; i++)
+            {
+                result.Append(arr[random.Next(37)]);
+            }
+            return result.ToString();
+        }
+    }
+}
